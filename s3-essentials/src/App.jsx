3b18fafc4ac036528acc8d9df95248c2accef3e5
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-12-14 13:09:58 +0600

    alternative props syntaxes-cot44_a-sohan-s3

--- s3-essentials/src/App.jsx
+++ s3-essentials/src/App.jsx
@@ -1,5 +1,6 @@
 import reactImg from './assets/react1.png'
-import componentsImg from './assets/react2.png'
+//import componentsImg from './assets/react2.png'
+import { CORE_CONCEPTS } from './data.js';
 
 const reactDescriptions = ['Fundamental','Crucial','Core'];
 
@@ -48,13 +49,25 @@ function App() {
         <h2>Core Concepts</h2>
         <ul>
           <CoreConcept 
-          title="Components"
-          description="The core UI building block by Sohan."
-          image={componentsImg}
+          title={CORE_CONCEPTS[0].title}
+          description={CORE_CONCEPTS[0].description}
+          image={CORE_CONCEPTS[0].image}
           />
-          <CoreConcept title="Props" />
-          <CoreConcept/>
-          <CoreConcept/>
+           <CoreConcept 
+          title={CORE_CONCEPTS[1].title}
+          description={CORE_CONCEPTS[1].description}
+          image={CORE_CONCEPTS[1].image}
+          />
+           <CoreConcept 
+          title={CORE_CONCEPTS[2].title}
+          description={CORE_CONCEPTS[2].description}
+          image={CORE_CONCEPTS[2].image}
+          />
+          <CoreConcept 
+          title={CORE_CONCEPTS[3].title}
+          description={CORE_CONCEPTS[3].description}
+          image={CORE_CONCEPTS[3].image}
+          />
         </ul>
         </section>
         
